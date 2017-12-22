06b7ea903a75fd19edce937ef2b7d2b1e8115729
ZoeyOneOhOne <cole.perry113@gmail.com>
2017-12-22 12:57:16 -0500

    idk

---
+++ HSVersion1/HSVersion1/Models/BakeryMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+namespace HSVersion1.Models
+{
+    public class BakeryMenu
+    {
+        public int ID { get; set; } 
+        public string Item { get; set; }
+        public string Description { get; set; }
+        public SqlMoney Price { get; set; }
+    }
+}
